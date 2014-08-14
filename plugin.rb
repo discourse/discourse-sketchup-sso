a94b7ff8a65b0d5069dd694714e4b30387e1607e
Sam <sam.saffron@gmail.com>
2014-08-14 15:14:33 +1000

    Custom authenticator now works in dev

--- plugin.rb
+++ plugin.rb
@@ -3,9 +3,31 @@
 # version: 0.1
 # authors: Sam Saffron
 
+class SketchupAuthenticator < Auth::OpenIdAuthenticator
+
+  def after_authenticate(auth_token)
+    result = super(auth_token)
+    info = auth_token[:info]
+
+    # For now lets debug the info in /logs, we can remove later
+    Rails.logger.warn("OpenID reply recieved" << info.inspect)
+
+    # TODO update whatever needs updating here
+    # if result.email.present? && result.user.present?
+    #   result.user.update_columns(email: result.email)
+    # end
+
+    result
+  end
+
+end
+
+url = Rails.env.development? ? 'https://dev-accounts.sketchup.com/openid/provider' :
+                               'https://accounts.sketchup.com/openid/provider'
+
 auth_provider :title => 'with Sketchup',
-              :authenticator => Auth::OpenIdAuthenticator.new(
-                'sketchup','https://accounts.sketchup.com/openid/provider',
+              :authenticator => SketchupAuthenticator.new(
+                'sketchup', url,
                 trusted: true),
               :message => 'Authenticating with Sketchup (make sure pop up blockers are not enabled)',
               :frame_width => 1000,   # the frame size used for the pop up window, overrides default
@@ -13,7 +35,7 @@ auth_provider :title => 'with Sketchup',
 
 
 register_css <<CSS
-.btn-social.ubuntu {
+.btn-social.sketchup {
   background: #00b;
 }
 
