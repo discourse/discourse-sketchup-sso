29eb38f2df80741b27d789a5a1f806848b8048a4
Jarek Radosz <jarek@cvx.dev>
2024-01-16 17:49:44 +0100

    DEV: Update linting (#3)

--- plugin.rb
+++ plugin.rb
@@ -1,24 +1,25 @@
+# frozen_string_literal: true
 # name: discourse-sketchup-sso
 # about: Sketchup SSO support
 # version: 0.2
 # authors: Sam Saffron, Roy Chanley
 
-require 'multi_json'
+require "multi_json"
 
 class SketchupAuthenticator < ::Auth::Authenticator
-
   def name
     "sketchup"
   end
 
   def register_middleware(omniauth)
     omniauth.provider :sketchup,
-      setup: lambda { |env|
-        opts = env["omniauth.strategy"].options
-        opts[:authorize_url] = SiteSetting.sketchup_authorize_url
-        opts[:sso_cookie_name] = SiteSetting.sketchup_sso_cookie_name
-        opts[:userinfo_url] = SiteSetting.sketchup_userinfo_url
-      }
+                      setup:
+                        lambda { |env|
+                          opts = env["omniauth.strategy"].options
+                          opts[:authorize_url] = SiteSetting.sketchup_authorize_url
+                          opts[:sso_cookie_name] = SiteSetting.sketchup_sso_cookie_name
+                          opts[:userinfo_url] = SiteSetting.sketchup_userinfo_url
+                        }
   end
 
   def after_authenticate(auth_token)
@@ -40,29 +41,23 @@ end
 class OmniAuth::Strategies::Sketchup
   include OmniAuth::Strategy
 
-  option :name, 'sketchup'
-  option :authorize_url, ''
-  option :sso_cookie_name, ''
-  option :userinfo_url, ''
+  option :name, "sketchup"
+  option :authorize_url, ""
+  option :sso_cookie_name, ""
+  option :userinfo_url, ""
 
-  uid do
-    @auth_data['trimbleid']
-  end
+  uid { @auth_data["trimbleid"] }
 
   info do
     {
-      name: "#{@auth_data['firstname']} #{@auth_data['lastname']}",
-      email: @auth_data['email'],
-      first_name: @auth_data['firstname'],
-      last_name: @auth_data['lastname']
+      name: "#{@auth_data["firstname"]} #{@auth_data["lastname"]}",
+      email: @auth_data["email"],
+      first_name: @auth_data["firstname"],
+      last_name: @auth_data["lastname"],
     }
   end
 
-  extra do
-    {
-      raw_info: @auth_data
-    }
-  end
+  extra { { raw_info: @auth_data } }
 
   def request_phase
     redirect(options.authorize_url)
@@ -75,24 +70,26 @@ class OmniAuth::Strategies::Sketchup
     # Note that if the cookie isn't part of the request above, it's likely
     # because you're not on a sketchup.com domain (and thus this will be broken).
     if request.cookies.has_key?(options.sso_cookie_name)
-      response = Excon.get(options.userinfo_url,
-        headers: { Authorization: request.cookies[options.sso_cookie_name].tr('"', '') })
+      response =
+        Excon.get(
+          options.userinfo_url,
+          headers: {
+            Authorization: request.cookies[options.sso_cookie_name].tr('"', ""),
+          },
+        )
 
       @auth_data = MultiJson.decode(response.body.to_s)
       super
-
     else
       fail!(:invalid_request)
     end
-
   rescue StandardError => e
     fail!(:invalid_response, e)
   end
-
 end
 
-auth_provider title: 'with SketchUp',
-              message: 'Authenticating with SketchUp (make sure your pop up blocker is disabled).',
+auth_provider title: "with SketchUp",
+              message: "Authenticating with SketchUp (make sure your pop up blocker is disabled).",
               frame_width: 725,
               frame_height: 600,
               authenticator: SketchupAuthenticator.new
